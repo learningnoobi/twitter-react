5d2f16f97fe91f0f09275461dec017d8c1eb7460
learningnoobi <fanime492@gmail.com>
2021-08-30 03:27:54 +0545

    :sparkles: minor fix

--- src/components/tweetComp/DropDown.jsx
+++ src/components/tweetComp/DropDown.jsx
@@ -1,32 +1,35 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { BiUserPlus, BiEditAlt, BiBlock } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
 import { useDispatch } from "react-redux";
 import { deleteTweet } from "../../redux/asyncActions/TweetAsync";
-const DropDown = ({ tweetId,user,tweet }) => {
-
+const DropDown = ({ tweetId, user, tweet }) => {
   const dispatch = useDispatch();
   const deletePost = (id) => {
     dispatch(deleteTweet(id));
   };
-  console.log(user)
+  console.log(user);
   return (
     <div className="dropdownMenu">
-      {user.username=== tweet.author.username ?null
-      :<p>
-      <BiUserPlus /> <span>Unfollow Rayon</span>
-    </p>
-      }
-      <p>
-        <BiBlock />
-        <span>Block</span>
-      </p>
-      <p onClick={() => deletePost(tweetId)}>
-        <AiOutlineDelete color="#e0245e" />
-        <span style={{ color: "#e0245e" }}>Delete Post</span>
-      </p>
-    </div>
+      {user.username === tweet.author.username ? 
+       <p onClick={() => deletePost(tweetId)}>
+       <AiOutlineDelete color="#e0245e" />
+       <span style={{ color: "#e0245e" }}>Delete Post</span>
+     </p>
+      : (
+        <>
+          <p>
+            <BiUserPlus /> <span>Unfollow Rayon</span>
+          </p>
+          <p>
+            <BiBlock />
+            <span>Block</span>
+          </p>
+        </>
+      )}
 
+     
+    </div>
   );
 };
 
